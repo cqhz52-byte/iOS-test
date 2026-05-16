29f30e3d830cb42d084951d19b54812e283a5b54
cqhz52-byte <cqhz52@gmail.com>
2026-05-16 12:46:21 +0800

    feat: update lianliankan web game assets

--- sw.js
+++ sw.js
@@ -5,7 +5,8 @@ const ASSETS = [
   "./styles.css",
   "./game.js",
   "./manifest.webmanifest",
-  "./icons/icon.svg"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
